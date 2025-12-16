a8aa18b52c698f1c66a9c274dfa9a35caccd3be7
NathanielOlayvar <mnolayvar01@tip.edu.ph>
2025-12-16 10:09:37 +0800

    configured views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User    
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -7,3 +13,8 @@ def health_check(request):
         "status": "ok",
         "app": "Advisuri System"
     })
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny] 
