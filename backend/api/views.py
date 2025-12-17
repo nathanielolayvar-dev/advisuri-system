d77d5483522fc753e25d6528fb8b267cee32f6c5
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2025-12-17 21:37:55 +0800

    views.py: create and delete notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User    
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,6 +14,29 @@ def health_check(request):
         "status": "ok",
         "app": "Advisuri System"
     })
+
+class NoteListCreate(generics.ListCreateAPIView): #class for creating notes
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) #note written by specific user
+    
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class NoteDelete(generics.DestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user) #note written by specific user
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
