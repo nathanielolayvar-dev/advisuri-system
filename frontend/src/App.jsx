61f955567280c7802b18b4b4f3607d2b783a8a9c
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-01-07 07:37:46 +0800

    update pages
    added the home, login, notfound, and register tentative pages.
    
    also add react-router-dom npm in requirements.txt and dependencies.txt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,37 @@
 import react from "react"
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoutes"
+
+function Logout() {
+  localStorage.clear() //clear access and refresh token 
+  return <Navigate to="/login" /> //redirect to login page
+}
+
+function RegisterAndLogout() {
+  localStorage.clear() 
+  return <Register />
+}
 
 function App() { 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes> 
+        <Route
+            path="/"
+            element={
+              <ProtectedRoute> {/* cannot access routes inside without the access token and validity */}
+                <Home />
+              </ProtectedRoute>
+            }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}> </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
