b5a139fe08f2ffd1420eb90facb315c0778e9c9f
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-01-07 11:26:34 +0800

    settings.py in backend/backend
    fixed the authentication and permission classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,12 +35,12 @@ ALLOWED_HOSTS = ["*"]
 
 #jwt token config
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 SIMPLE_JWT = {
