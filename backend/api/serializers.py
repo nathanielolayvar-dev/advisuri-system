87675da005f9f8e575d185d3146efde04fbb7aea
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-01-07 15:04:35 +0800

    home.jsx notes
    home.jsx update

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -16,5 +16,5 @@ class UserSerializer(serializers.ModelSerializer):
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["id", "title", "content", "created-at", "author"]
+        fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,10 @@ function Home() {
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
 
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   const getNotes = () => {
     api
       .get('/api/notes/')
@@ -17,11 +21,60 @@ function Home() {
       .catch((err) => alert(err));
   };
 
-  useEffect(() => {
-    getNotes();
-  }, []);
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert('Note deleted!');
+        else alert('Failed to delete note.');
+        getNotes();
+      })
+      .catch((error) => alert(error));
+  };
 
-  return <div>Home</div>;
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post('/api/notes/', { content, title })
+      .then((res) => {
+        if (res.status === 201) alert('Note created!');
+        else alert('failed to make a note.');
+        getNotes();
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>Notes</h2>
+      </div>
+      <h2> Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          id="content"
+          name="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
