08bbe816c796e9b79b6e5ccc7d554e06b0c1d413
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-01-08 16:11:44 +0800

    env gitignores
    add gitignores to not include database environment vaiables

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,7 +3,7 @@ import api from '../api';
 import { useNavigate } from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
 import '../styles/form.css';
-import loadingIndicator from './LoadingIndicator';
+import LoadingIndicator from './LoadingIndicator';
 
 function Form({ route, method }) {
   const [username, setUsername] = useState('');
