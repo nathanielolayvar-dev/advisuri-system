211d98f8e6c0708ec0ecd900c137a7025becde37
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-01-08 15:48:19 +0800

    database setup
    used supabase
    
    commands:
    - python manage.py migrate
    - python manage.py runserver
    
    in settings.py, configured from dbsqlite to postgresql and necessary credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,6 +17,7 @@ import os
 
 #call funtion 
 load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,8 +107,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.getenv("DB_NAME"), #can be located in .env in this folder
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
