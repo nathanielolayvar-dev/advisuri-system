e51bfb14edfc5d293c53ae28ecc7e723fbb56d40
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-03-27 08:52:21 +0800

    urls.py import
    removed send_report import in urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
-from .views import send_report
 
 router = DefaultRouter()
 router.register(r'messages', views.MessageViewSet, basename='message')
