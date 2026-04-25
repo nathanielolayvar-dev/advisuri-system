4f5ac132578f320d1136efbd8e98d16798886b6d
nathanielolayvar-dev <nathanielolayvar06@gmail.com>
2026-04-25 11:08:05 +0800

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+
+# This line tells Python to look 
+# inside the current folder for the 'advisuri' module
+# Get the path of the 'backend' folder
+path = os.path.dirname(os.path.abspath(__file__))
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advisuri.settings')
 
 application = get_wsgi_application()
